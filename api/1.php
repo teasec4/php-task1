9d7df95b9e139e852f927eaa215c7a447de8d615
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-06-16 02:31:42 +0500

    Add files via upload

---
+++ api/1.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+    $a = ($_GET['a']);
+    $b = ($_GET['b']);
+    $c = ($_GET['c']);
+?>
+
+<form method="POST" action="2.php">
+    <p>
+        a: <input type="text" name='a' value="<?php echo $a ?>"/>
+    </p>
+    <p>
+        b: <input type="text" name='b' value="<?php echo $b ?>"/>
+    </p>
+    
+    <p>
+        <input type="submit" value="ok"/>
+    </p>
+</form>
+<?php echo $c ?>
+</body>
+</html>
+
---
+++ api/2.php
@@ -0,0 +1,16 @@
+<?php 
+$a =  htmlspecialchars($_POST['a']);
+$b =  htmlspecialchars($_POST['b']);
+$c =  htmlspecialchars($_POST['c']);
+$answer = $a * $b + $c;
+$_GET = $answer;
+?>
+
+
+<?php echo 'a = ' . $a; ?><br>
+<?php echo 'b = ' . $b; ?><br>
+<?php echo 'c = ' . $c; ?><br>
+<?php echo 'a * b + c = ' . $answer; ?>
+
+
+
