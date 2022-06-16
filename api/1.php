fa3c4cb242e6a517586c25783c6223b4d20eb296
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-06-16 10:13:31 +0500

    Add files via upload

--- api/1.php
+++ api/1.php
@@ -8,10 +8,17 @@
     echo "b = $b <br>";
     echo "c = $c <br>";
 
+    $array = array(
+        'a' => $a,
+        'b' => $b,
+        'c' => $c
+    );
+
+
     $ch = curl_init();
 
-    //curl_setopt($ch, CURLOPT_URL, "https://php-task1.vercel.app/api/2.php?a=$a&b=$b&c=$c");
-
+    curl_setopt($ch, CURLOPT_URL, "https://php-task1.vercel.app/api/2.php");
+    curl_setopt($ch, CURLOPT_POST, http_build_query($array,'', '&'));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $server_response = curl_exec($ch);
